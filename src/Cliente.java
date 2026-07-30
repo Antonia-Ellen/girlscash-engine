5193eb32e40960a627b8033d321c656d13289fb4
Antonia Ellen Karoliny Sousa Silva <antoniaellenkaroliny@gmail.com>
2026-07-29 22:08:14 -0300

    feat: setup inicial do GirlsCash com classes Cliente e Application

---
+++ src/Cliente.java
@@ -0,0 +1,70 @@
+public class Cliente {
+    //proteger pra que ninguém altere os dados fora dessa classe
+    private String nome;
+    private String cpf;
+    private int score;
+    private boolean negativado;
+
+    //construtor
+    public Cliente() {
+        nome = "Ellen";
+        cpf = "123.456.789";
+        score = 601;
+        negativado = false;
+    }
+
+    public Cliente(String nome, String cpf, int score, boolean negativado) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.score = score;
+        this.negativado = negativado;
+    }
+
+    //libera outra classe a ver o nome
+    public String getNome() {
+        return nome;
+    }
+
+    //libera outra classe a alterar o nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCPF(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isNegativado() {
+        return negativado;
+    }
+
+    public void setNegativado(boolean negativado) {
+        this.negativado = negativado;
+    }
+
+    //não precisa adicionar variavel de parametro porque estamos dentro de cliente
+    //se fosse fora iria precisar (boolean negativado, int score)
+    public boolean eElegivelParaCredito() {
+        /*if (!negativado && score >= 600) {
+            return true;
+        } else {
+            return false;
+        }
+
+         */
+        //retorna o sim ou não se não for negativado e o score for maior ou igual a 600
+    return !negativado && score >= 600;
+    }
+}
---
+++ src/GirlsCashApplication.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+public class GirlsCashApplication {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("✨ Bem-vinda ao GirlsCash-Engine ✨\n");
+        // [Etapa 01] Cálculo de Encargos e Juros do Rotativo
+        double saldoInicial = 50.00;
+        double juros = saldoInicial * 0.03; //3% de juros
+        double multa = saldoInicial * 0.02; //2% de multa
+
+        // Valor total atualizado da fatura com os encargos
+        double saldoTotalAtualizado = saldoInicial + juros + multa;
+
+        //Menu com os dados do extrato do cliente
+        System.out.println("💸 Saldo Inicial: R$ " + saldoInicial);
+        System.out.println("📈 Encargos (Juros + Multa): R$ " + (juros + multa));
+        System.out.println("💖 Saldo Devedor Atualizado GirlsCash: R$ " + saldoTotalAtualizado);
+
+        //etapa 2 Análise de Risco
+
+        //colocar se está com o nome negativado
+        System.out.println("Nome negativado?: 1 sim e 2 não)");
+        int nomeNegativado = leitor.nextInt();
+
+        //assegurar que o número está entre 1 e 2
+        while (nomeNegativado < 1 || nomeNegativado > 2) {
+            System.out.println("Nome negativado : 1 sim e 2 não)");
+            nomeNegativado = leitor.nextInt();
+        }
+
+        if (nomeNegativado == 1) {
+            System.out.println("Alto Risco! Processo de análise interrompido! Limpe seu nome!");
+        } else {
+            System.out.println("Baixo Risco. Está liberado para análise do score!");
+
+            //etapa 3 Faixas de limite
+
+            System.out.println("Digite seu score (apenas números!)");
+            int scoreDigitado = leitor.nextInt();
+
+            //Verificar se o valor de score está dentro dos padrões(0 a 1000)
+            while (scoreDigitado < 0 || scoreDigitado > 1000) {
+                //Score inválido! Bloqueia a entrada na regra para que ocorra a correção do valor
+                System.out.println("Score inválido! Digite seu score (apenas números entre zero e mil!)");
+                scoreDigitado = leitor.nextInt();
+            }
+
+            //Score válido! agora vai passar pela regra de negócio (baixo, médio e alto risco))
+            if (scoreDigitado >= 750) {
+                System.out.println("Limite alto liberado !");
+            } else if (scoreDigitado >= 600) {
+                System.out.println("Limite médio liberado !");
+            } else {
+                System.out.println("Liberação recusada. Alto risco!!!");
+            }
+
+            //se o score estiver dentro do aceitavel pela empresa
+            //vamos liberar o cédito
+            if (scoreDigitado >= 600 && scoreDigitado <= 1000 && nomeNegativado == 2) {
+                //ETAPA 4:
+                // Escolha do produto
+                System.out.println("Digite o produto que deseja 1,2 ou 3:");
+                System.out.println("1 - Empréstimo Consignado GirlsCash (Taxa: 1.9% a.m.)");
+                System.out.println("2 - Empréstimo Pessoal GirlsCash (Taxa: 4.5% a.m.)");
+                System.out.println("3 - Cartão de Crédito GirlsCash Black (Anuidade Grátis + Cashback)");
+                int opcaoEscolhida = leitor.nextInt();
+
+                //menu com as 3 opções de produto
+                while (opcaoEscolhida < 1 || opcaoEscolhida > 3) {
+                    System.out.println("Digite o produto que deseja 1,2 ou 3:");
+                    System.out.println("1 - Empréstimo Consignado GirlsCash (Taxa: 1.9% a.m.)");
+                    System.out.println("2 - Empréstimo Pessoal GirlsCash (Taxa: 4.5% a.m.)");
+                    System.out.println("3 - Cartão de Crédito GirlsCash Black (Anuidade Grátis + Cashback)");
+                    opcaoEscolhida = leitor.nextInt();
+                }
+                switch (opcaoEscolhida) {
+                    case 1:
+                        System.out.println("Empréstimo Consignado GirlsCash (Taxa: 1.9% a.m.)");
+                        break;
+                    case 2:
+                        System.out.println("Empréstimo Pessoal GirlsCash (Taxa: 4.5% a.m.)");
+                        break;
+                    case 3:
+                        System.out.println("Cartão de Crédito GirlsCash Black (Anuidade Grátis + Cashback)");
+                        break;
+                }
+
+                //Autenticação do cliente antes de realizar a compra
+                int pinCorreto = 1234;
+                int pinDigitado;
+                System.out.println("Digite sua senha de 4 dígitos (apenas números!)");
+                pinDigitado = leitor.nextInt();
+                //enquanto a senha estiver incorreta o crédito não será liberado
+                while (pinCorreto != pinDigitado) {
+                    System.out.println("Senha incorreta! Digite novamente!");
+                    pinDigitado = leitor.nextInt();
+                }
+                System.out.println("Senha correta!");
+
+                // ETAPA 5
+                // Carnê
+                //Define o valor da venda
+                System.out.println("CARNÊ:");
+                System.out.println("Digite o valor da compra:");
+                double valor = leitor.nextDouble();
+
+                //Calcular o valor da parcela
+                double calculoParcela = valor / 6;
+                // exibir o carnê
+                for (int i = 1; i <= 6; i++) {
+                    String formatadoValor = String.format("%.2f", calculoParcela);
+                    System.out.println(" Parcela " + i + "/6 - Valor: R$ " + formatadoValor + " | Status: A vencer");
+                }
+            }
+        }
+    }
+}
